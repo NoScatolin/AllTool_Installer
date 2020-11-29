d9f579f8c1290bf517c6d3a3755d7f8762c3434b
Matheus Scatolin <60984217+NoScatolin@users.noreply.github.com>
2020-11-29 18:44:20 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo -e '\e[36m[+] Instalando as seguintes ferramentas: \e[39m'
+echo ''
+echo -e '\e[36m ◉ Net-Tools, Nmap, Telnet, cURL, NetCat, Plank, SQLmap, Java, Python, Golang, Pip, Impacket, aMASS, Enum4Linux, HTTProbe, EyeWitness, massRECON, John, AirCrack-NG, Extract, ExifTool, WhoIs, Dirb... \e[39m'
+echo ''
+
+
+## Efetuando os downloads e a instalação das ferramentas ##
+apt update && apt upgrade -y
+apt autoremove -y
+apt install net-tools nmap telnet curl netcat plank sqlmap default-jre default-jdk python3 golang john aircrack-ng extract exiftool whois dirb -y
+apt update && apt upgrade -y
+apt autoremove -y
+apt install python3-pip
+pip3 install impacket
+snap install amass
+
+
+## Indo para o diretório /usr/share ##
+cd /usr/share/
+
+
+## Fazendo o download das ferramentas a partir do GitHub ##
+git clone https://github.com/CiscoCXSecurity/enum4linux
+git clone https://github.com/tomnomnom/httprobe
+git clone https://github.com/FortyNorthSecurity/EyeWitness
+git clone https://github.com/noscatolin/massRECON
+
+
+## Efetuando a instalação do HTTProbe ##
+cd httprobe
+go build main.go
+mv main httprobe
+
+
+## Efetuando a instalação do EyeWitness ##
+cd ..
+mv EyeWitness/ eyewitness
+
+
+## Adicionando os alias das ferramentas no bashrc ##
+echo "alias httprobe='cd /usr/share/httprobe/ && ./httprobe'
+alias enum4linux='cd /usr/share/enum4linux/ && perl enum4linux.pl'
+alias eyewitness='cd /usr/share/eyewitness/Python/ && python3 EyeWitness.py'
+alias recon='cd /usr/share/massRECON/ && bash recon.sh'" >> ~/.bashrc
+
+
+## Efetuando a última atualização ##
+apt update && apt upgrade -y
+apt autoremove -y
+
+echo -e '\e[36m[*] Instalações e Atualizações concluídas! \e[39m'
